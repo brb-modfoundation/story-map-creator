6ac0a0ff1421ece8f58f8aa8e54ec60efe601768
brb-modfoundation <resilientbengaluru@gmail.com>
2026-06-23 11:01:35 +0530

    Publish story map: test (test-mqghwwev)

---
+++ published/test-mqghwwev/storyConfig.js
@@ -0,0 +1,254 @@
+const storyConfig = {
+  "chapters": [
+    {
+      "id": "0",
+      "year": null,
+      "zoom": 11.05,
+      "image": null,
+      "pitch": 51,
+      "quote": null,
+      "title": "Inhabiting the Flow",
+      "center": [
+        77.583622,
+        12.941649
+      ],
+      "layers": {
+        "layer-gba-boundary-fill": {
+          "color": "#0d6aff",
+          "opacity": 0.1,
+          "visible": true
+        },
+        "layer-gba-boundary-outline": {
+          "color": "#ff00c8",
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 3.5
+        }
+      },
+      "bearing": 30,
+      "duration": 2000,
+      "subtitle": "Pre- Colonisation",
+      "alignment": "center",
+      "buttonUrl": null,
+      "subtitle2": "Social and Cultural Associations with Stormwater",
+      "buttonText": null,
+      "population": null,
+      "chapterType": "title",
+      "description": null,
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    },
+    {
+      "id": "1",
+      "year": null,
+      "zoom": 11.25,
+      "image": null,
+      "pitch": 0,
+      "quote": null,
+      "title": "Inscription Stones",
+      "center": [
+        77.624604,
+        12.991886
+      ],
+      "layers": {
+        "layer-dem-cog": {
+          "color": "#ffffff",
+          "opacity": 0.35,
+          "visible": true,
+          "strokeWidth": 2
+        },
+        "layer-gba-boundary-fill": {
+          "color": "#ff0f0f",
+          "opacity": 0,
+          "visible": true
+        },
+        "layer-gba-boundary-outline": {
+          "color": "#ffffff",
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 1
+        },
+        "layer-lake-inscription-stones": {
+          "color": "#fff700",
+          "radius": 4,
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 2
+        }
+      },
+      "bearing": 0,
+      "duration": 2000,
+      "subtitle": null,
+      "alignment": "center",
+      "buttonUrl": null,
+      "subtitle2": null,
+      "buttonText": null,
+      "population": null,
+      "chapterType": "map",
+      "description": "",
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    },
+    {
+      "id": "2",
+      "year": "AD 870",
+      "zoom": 15.91,
+      "image": "./images/chapter-2-image.jpg",
+      "pitch": 0,
+      "quote": "The Irvvuliyur odeya, Irugamayya’s son Sirimayya, fixed sluices to the two tanks, had the eastern tank built, and obtained the bittuvatta of the three tanks.",
+      "title": "The Bittuvatta",
+      "center": [
+        77.642434,
+        12.920304
+      ],
+      "layers": {
+        "3-label": {
+          "opacity": 1,
+          "visible": true
+        },
+        "layer-gba-boundary-fill": {
+          "color": "#ff0f0f",
+          "opacity": 0,
+          "visible": true
+        },
+        "layer-gba-boundary-outline": {
+          "color": "#ffffff",
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 1
+        },
+        "layer-lake-inscription-stones": {
+          "color": "#fff700",
+          "radius": 4,
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 2
+        }
+      },
+      "bearing": 0,
+      "duration": 8000,
+      "subtitle": null,
+      "alignment": "left",
+      "buttonUrl": null,
+      "subtitle2": null,
+      "buttonText": null,
+      "population": null,
+      "chapterType": "map",
+      "description": "An important motivation for the construction of lakes was the granting of bittuvatta , agricultural grants of irrigated lands below the lake, to the founder. Lakes were not isolated entities, and along with them, grants frequently included dry lands above the lake, irrigated lands below the lake, and trees and wells in the vicinity.\n\nAn inscriptions stone near Agara Lake dating back to AD 870 reads one such grant.",
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    },
+    {
+      "id": "3",
+      "year": "AD 1370",
+      "zoom": 15.17,
+      "image": "./images/chapter-3-image.png",
+      "pitch": 0,
+      "quote": "Having cleared the jungle in the tract of land adjoining Peru-Erumur, ? leveled the ground, built a village, constructed a tank by removing the sand, and named the village Vachchidevarpuram, granted to Vacchi-devar the village and the wet and dry lands adjoining it, with their four boundaries, as a madappuram, exempt from taxes, for as long as the moon and the sun exist. \n",
+      "title": "The Bittuvatta",
+      "center": [
+        77.677324,
+        12.967736
+      ],
+      "layers": {
+        "4-label": {
+          "opacity": 1,
+          "visible": true
+        },
+        "layer-gba-boundary-fill": {
+          "color": "#ff0f0f",
+          "opacity": 0,
+          "visible": true
+        },
+        "layer-gba-boundary-outline": {
+          "color": "#ffffff",
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 1
+        },
+        "layer-lake-inscription-stones": {
+          "color": "#fff700",
+          "radius": 4,
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 2
+        }
+      },
+      "bearing": 0,
+      "duration": 8000,
+      "subtitle": null,
+      "alignment": "right",
+      "buttonUrl": null,
+      "subtitle2": null,
+      "buttonText": null,
+      "population": null,
+      "chapterType": "map",
+      "description": "",
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    },
+    {
+      "id": "4",
+      "year": null,
+      "image": "./images/chapter-4-image.jpg",
+      "quote": null,
+      "title": "New Image Chapter",
+      "layers": {},
+      "subtitle": null,
+      "alignment": "center",
+      "buttonUrl": null,
+      "subtitle2": null,
+      "buttonText": null,
+      "population": null,
+      "chapterType": "image",
+      "description": "",
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    },
+    {
+      "id": "5",
+      "year": null,
+      "image": "./images/chapter-5-image.jpg",
+      "quote": null,
+      "title": "New Image Chapter",
+      "layers": {},
+      "subtitle": null,
+      "alignment": "center",
+      "buttonUrl": null,
+      "subtitle2": null,
+      "buttonText": null,
+      "population": null,
+      "chapterType": "image",
+      "description": "",
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    },
+    {
+      "id": "6",
+      "year": null,
+      "image": "./images/chapter-6-image.jpg",
+      "quote": null,
+      "title": "New Image Chapter",
+      "layers": {},
+      "subtitle": null,
+      "alignment": "center",
+      "buttonUrl": null,
+      "subtitle2": null,
+      "buttonText": null,
+      "population": null,
+      "chapterType": "image",
+      "description": "",
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    }
+  ]
+};
+
+if (typeof module !== 'undefined' && module.exports) { module.exports = storyConfig; }
