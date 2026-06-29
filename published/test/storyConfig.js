dd84d93ad1b57e669e1944ff599de0fa386d6297
brb-modfoundation <resilientbengaluru@gmail.com>
2026-06-29 11:25:03 +0530

    Publish story map: test (test)

---
+++ published/test/storyConfig.js
@@ -0,0 +1,273 @@
+const storyConfig = {
+  "chapters": [
+    {
+      "id": "0",
+      "year": null,
+      "zoom": 11.38,
+      "image": null,
+      "pitch": 0,
+      "quote": null,
+      "title": "DRAWING BORDERS DRAINING WATER",
+      "center": [
+        77.589557,
+        12.988139
+      ],
+      "layers": {
+        "layer-dem-cog": {
+          "color": "#ffffff",
+          "opacity": 1,
+          "visible": true,
+          "strokeColor": "#0d6aff",
+          "strokeWidth": 2
+        },
+        "layer-wetlands-fill": {
+          "color": "#0064ff",
+          "opacity": 1,
+          "visible": true
+        },
+        "layer-1870-final-cog": {
+          "color": "#ffffff",
+          "opacity": 1,
+          "visible": true,
+          "strokeColor": "#0d6aff",
+          "strokeWidth": 2
+        },
+        "layer-cantonment-cog": {
+          "color": "#ffffff",
+          "opacity": 1,
+          "visible": true,
+          "strokeColor": "#0d6aff",
+          "strokeWidth": 2
+        },
+        "layer-gba-wards-fill": {
+          "color": "#0064ff",
+          "opacity": 1,
+          "visible": true
+        },
+        "layer-wetlands-outline": {
+          "color": "#0d6aff",
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 1
+        },
+        "layer-gba-wards-outline": {
+          "color": "#0d6aff",
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 1
+        }
+      },
+      "legend": null,
+      "bearing": 0,
+      "duration": 2000,
+      "subtitle": null,
+      "alignment": "center",
+      "buttonUrl": null,
+      "subtitle2": null,
+      "buttonText": null,
+      "population": null,
+      "chapterType": "title",
+      "description": null,
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    },
+    {
+      "id": "1",
+      "year": null,
+      "zoom": 13.03,
+      "image": null,
+      "pitch": 0,
+      "quote": null,
+      "title": "New Chapter",
+      "center": [
+        77.60472,
+        12.974285
+      ],
+      "layers": {
+        "layer-1870-final-cog": {
+          "color": "#ffffff",
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 2
+        }
+      },
+      "legend": null,
+      "bearing": 0,
+      "duration": 2000,
+      "subtitle": null,
+      "alignment": "center",
+      "buttonUrl": null,
+      "subtitle2": null,
+      "buttonText": null,
+      "population": null,
+      "chapterType": "map",
+      "description": "After the fall of Tipu Sultan and the capture of Bengaluru’s Fort during the Fourth Anglo-Mysore War in 1799, the British decided that Srirangapatna was no longer suitable for them.\n\n<light>Bangalore’s climate had long attracted the attention of the East India Company, and the British were drawn to shift their forces here.</light>",
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    },
+    {
+      "id": "2",
+      "year": null,
+      "zoom": 12.29,
+      "image": null,
+      "pitch": 0,
+      "quote": null,
+      "title": "New Chapter",
+      "center": [
+        77.609111,
+        12.971855
+      ],
+      "layers": {
+        "layer-wetlands-fill": {
+          "color": "#4fff0f",
+          "opacity": 0.25,
+          "visible": true
+        },
+        "layer-wetlands-outline": {
+          "color": "#20acb6",
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 3.5
+        }
+      },
+      "legend": null,
+      "bearing": 0,
+      "duration": 2000,
+      "subtitle": null,
+      "alignment": "left",
+      "buttonUrl": null,
+      "subtitle2": null,
+      "buttonText": null,
+      "population": null,
+      "chapterType": "map",
+      "description": "After the fall of Tipu Sultan and the capture of Bengaluru’s Fort during the Fourth Anglo-Mysore War in 1799, the British decided that Srirangapatna was no longer suitable for them.\n\n<light>Bangalore’s climate had long attracted the attention of the East India Company, and the British were drawn to shift their forces here.</light>",
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    },
+    {
+      "id": "3",
+      "year": null,
+      "zoom": 14.53,
+      "image": null,
+      "pitch": 0,
+      "quote": null,
+      "title": "New Chapter",
+      "center": [
+        77.617945,
+        12.982866
+      ],
+      "layers": {
+        "layer-wetlands-fill": {
+          "color": "#4fff0f",
+          "opacity": 0.25,
+          "visible": true
+        },
+        "layer-wetlands-outline": {
+          "color": "#20acb6",
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 3.5
+        }
+      },
+      "legend": null,
+      "bearing": 0,
+      "duration": 2000,
+      "subtitle": null,
+      "alignment": "center",
+      "buttonUrl": null,
+      "subtitle2": null,
+      "buttonText": null,
+      "population": null,
+      "chapterType": "map",
+      "description": "After the fall of Tipu Sultan and the capture of Bengaluru’s Fort during the Fourth Anglo-Mysore War in 1799, the British decided that Srirangapatna was no longer suitable for them.\n\n<light>Bangalore’s climate had long attracted the attention of the East India Company, and the British were drawn to shift their forces here.</light>",
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    },
+    {
+      "id": "4",
+      "year": null,
+      "zoom": 13.03,
+      "image": "./images/chapter-4-image.png",
+      "pitch": 0,
+      "quote": null,
+      "title": "New Chapter",
+      "center": [
+        77.60472,
+        12.974285
+      ],
+      "layers": {
+        "layer-wetlands-fill": {
+          "color": "#4fff0f",
+          "opacity": 0.25,
+          "visible": true
+        },
+        "layer-wetlands-outline": {
+          "color": "#20acb6",
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 3.5
+        }
+      },
+      "legend": "https://ifwmzmcnozyyavjjyqrn.supabase.co/storage/v1/object/public/datasets/708420c6-89b3-4c4f-8a73-2ee40ffa8b71/1782712358975-dem-legend.png",
+      "bearing": 0,
+      "duration": 2000,
+      "subtitle": null,
+      "alignment": "left",
+      "buttonUrl": null,
+      "subtitle2": null,
+      "buttonText": null,
+      "population": null,
+      "chapterType": "map",
+      "description": "After the fall of Tipu Sultan and the capture of Bengaluru’s Fort during the Fourth Anglo-Mysore War in 1799, the British decided that Srirangapatna was no longer suitable for them.\n\n<light>Bangalore’s climate had long attracted the attention of the East India Company, and the British were drawn to shift their forces here.</light>",
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    },
+    {
+      "id": "5",
+      "year": null,
+      "zoom": 13.03,
+      "image": null,
+      "pitch": 0,
+      "quote": null,
+      "title": "New Chapter",
+      "center": [
+        77.60472,
+        12.974285
+      ],
+      "layers": {
+        "layer-wetlands-fill": {
+          "color": "#4fff0f",
+          "opacity": 0.25,
+          "visible": true
+        },
+        "layer-wetlands-outline": {
+          "color": "#20acb6",
+          "opacity": 1,
+          "visible": true,
+          "strokeWidth": 3.5
+        }
+      },
+      "legend": null,
+      "bearing": 0,
+      "duration": 2000,
+      "subtitle": null,
+      "alignment": "right",
+      "buttonUrl": null,
+      "subtitle2": null,
+      "buttonText": null,
+      "population": null,
+      "chapterType": "map",
+      "description": "After the fall of Tipu Sultan and the capture of Bengaluru’s Fort during the Fourth Anglo-Mysore War in 1799, the British decided that Srirangapatna was no longer suitable for them.\n\n<light>Bangalore’s climate had long attracted the attention of the East India Company, and the British were drawn to shift their forces here.</light>",
+      "quoteSource": null,
+      "imageCaption": null,
+      "descriptionSource": null
+    }
+  ]
+};
+
+if (typeof module !== 'undefined' && module.exports) { module.exports = storyConfig; }
